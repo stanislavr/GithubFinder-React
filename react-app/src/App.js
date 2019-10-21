25c63d1702fcab1a6fd9c7b6b7ad82e1977cea32
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-10-20 22:46:04 -0400

    created an UserItem components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
