c425fe474aee0e1c8516bfbb26b46e8957ef1d85
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-10-22 23:03:16 -0400

    Using Router and Switch created About page

---
+++ react-app/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>About This App</h1>
+        <p>App to lookup Github users</p>
+        <p>Version 2.0.0</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default About;
